25d0898d8f57e41371ec3b76c7e01370e820336c
大江山岚 <554357332@qq.com>
2024-08-19 15:05:07 +0800

    changed View extens name

--- Sources/GBlur/GBlur.swift
+++ Sources/GBlur/GBlur.swift
@@ -4,7 +4,7 @@
 import SwiftUI
 
 extension View {
-    public func glur(radius: CGFloat = 8.0) -> some View {
+    public func gblur(radius: CGFloat = 8.0) -> some View {
         assert(radius >= 0.0, "Radius must be greater than or equal to 0")
         
         if #available(iOS 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, *) {
