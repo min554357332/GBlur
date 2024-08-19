1140aeba19dab7c7293e72b6486c5788e5a75ff7
大江山岚 <554357332@qq.com>
2024-08-19 15:04:10 +0800

    add View extens

--- Sources/GBlur/GBlur.swift
+++ Sources/GBlur/GBlur.swift
@@ -1,2 +1,16 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+extension View {
+    public func glur(radius: CGFloat = 8.0) -> some View {
+        assert(radius >= 0.0, "Radius must be greater than or equal to 0")
+        
+        if #available(iOS 17.0, macOS 14.0, tvOS 17.0, visionOS 1.0, *) {
+            return modifier(GBlurModifier(radius: radius))
+        } else {
+            return modifier(CompatibleModifier(radius: radius))
+        }
+    }
+}
